bd5c0b09ef2c558bb333e7801a01a8ada25e1d90
ryquo1 <adelcourt40@gmail.com>
2023-09-16 19:09:28 +0200

    Coreection d' Indentation et de nom de variable

--- script/script.js
+++ script/script.js
@@ -1,15 +1,15 @@
 let workDef = document.getElementById("work").textContent
 let breakDef = document.getElementById("break").textContent
-let bSart = document.getElementById("button")
+let buttonSart = document.getElementById("button")
 
 let workTime = 25
 let breakTime = 5
-let seconde = 0
+let second = 0
 let stopTime = 0;
 
 let isStart = true;
 
-bSart.addEventListener('click', ()=>{
+buttonSart.addEventListener('click', ()=>{
 
     if(isStart == true){
         document.getElementById("button").innerHTML = '<em class="fa-solid fa-arrow-rotate-left"></em>';
@@ -32,16 +32,16 @@ function start(){
 
     let workMinute = workTime-1;
     let breakMinute = breakTime -1;
-    seconde = 59;
+    second = 59;
     let count = false;
     
 
 let timerDecrease = () =>{
     document.getElementById('minute').textContent = workMinute;
-    document.getElementById('second').textContent = seconde;
-    seconde--
+    document.getElementById('second').textContent = second;
+    second--
 
-    if(seconde === 0){
+    if(second === 0){
         workMinute--
         if(workMinute === -1){
             if(!count){
@@ -54,7 +54,7 @@ let timerDecrease = () =>{
         count = false
         }
     }
-    seconde = 59;
+    second = 59;
 }
 
 } 
@@ -63,9 +63,9 @@ stopTime = setInterval(timerDecrease, 1000);
 
 function reset(){
     clearInterval(stopTime);
-    seconde = 0
+    second = 0
     document.getElementById('minute').textContent = workTime;
-    document.getElementById('second').textContent = seconde +"0";
+    document.getElementById('second').textContent = second +"0";
     document.getElementById('work').style.display = 'block'
     document.getElementById('break').style.display = 'block'
 }
